d1952514e9c6852c4df22ec6900980333b46a65f
bunnycast <berzzubunny@gmail.com>
2020-08-17 13:56:49 +0900

    refactor: AbstractUser > AbstractBaseUser,User MTM:Relation table

--- app/members/models.py
+++ app/members/models.py
@@ -1,19 +1,78 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.db import models
 
 
-class User(AbstractUser):
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-    email = models.EmailField(unique=True)
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True,
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
     relation_users = models.ManyToManyField(
         'self',
         through='Relations',
+        # relations_users 에 대한 역방향 참조에 대해서 거부한다.
         related_name='+',
-        symmetrical=False,
-        blank=True,
     )
 
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # The Simplest possible answer : Yes, Always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app 'app_label'?"
+        # The Simplest possible answer : Yes, Always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # The Simplest possible answer : All admins aer staff
+        return self.is_admin
+
+    def save(self, *args, **kwargs):
+        self.set_password(self.password)
+        return super().save(*args, **kwargs)
 
 class Relations(models.Model):
     CHOICE_RELATIONS_TYPE = (
@@ -36,3 +95,10 @@ class Relations(models.Model):
         max_length=10,
         choices=CHOICE_RELATIONS_TYPE,
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together=(
+            ('from_user', 'to_user'),
+            ('to_user', 'from_user'),
+        )
--- app/members/serializers.py
+++ app/members/serializers.py
@@ -12,11 +12,3 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
-
-    def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
