861b99d28d7a6c6a8b08324d102a8a7f7f5028a6
bunnycast <berzzubunny@gmail.com>
2020-08-19 15:53:15 +0900

    feat: drf_nested_router

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,10 +1,18 @@
+from django.conf.urls import url
+from django.urls import include
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
 from members.views import UserModelViewSet
+from posts.views import PostModelViewSet
 
 router = SimpleRouter(trailing_slash=False)
 router.register(r'users', UserModelViewSet)
-# router.register(r'relations', RelationModelViewSet)
 
+users_router = routers.NestedSimpleRouter(router, r'users', lookup='user')
+users_router.register(r'posts', PostModelViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^', include(users_router.urls)),
+]
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from posts.models import Post
+from posts.serializers import PostSerializer
+
+
+class PostModelViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
