a788fc799f4915ce4d51a4f1fe0bc1fd014052f6
bunnycast <berzzubunny@gmail.com>
2020-08-20 01:01:15 +0900

    feat: post: media 설정

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,13 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from rest_framework import viewsets
 
 from posts.models import Post
 from posts.serializers import PostSerializer
 
+User = get_user_model()
+
 
 class PostModelViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        qs = Post.objects.filter(user=User.objects.first())
+        return qs
 
     def perform_create(self, serializer):
         serializer.save()
